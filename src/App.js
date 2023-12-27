9d236f3aa25968f802730c3811d713e115ed6d09
vishnu gupta <thenormvg@gmail.com>
2023-12-28 03:02:44 +0530

    fine tuned draft 2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <HomePage/>
+      <Analytics />
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,10 +8,10 @@ export default function Projects() {
     <div>
         <h1>My Projects</h1>
         <div id='pj-list'>
-            <ProjectsCard name="nono" disc="this is my nono projects" link="https://thenorm.netlify.app" img="https://daddyjs.vercel.app/static/logo.svg"/>
-            <ProjectsCard name="nono" disc="this is my nono projects" link="https://thenorm.netlify.app" img="https://daddyjs.vercel.app/static/logo.svg"/>
-            <ProjectsCard name="nono" disc="this is my nono projects" link="https://thenorm.netlify.app" img="https://daddyjs.vercel.app/static/logo.svg"/>
-            <ProjectsCard name="nono" disc="this is my nono projects" link="https://thenorm.netlify.app" img=""/>
+            <ProjectsCard name="Batman ChatRoom" disc="Batman 2022 inspired terminal like web Chatroom" link="https://batmanchatroom.onrender.com/" />
+            <ProjectsCard name="Dusic" disc="A youtube based music player which I made because I hated ads" link="https://dusic.netlify.app/" />
+            <ProjectsCard name="ChatBotGpt" disc="A gpt-2 based custom chatbot which can be finetuned" link="https://github.com/NormVg/ChatBotGpt" />
+            <ProjectsCard name="DevG" disc="A general purpose everyday developer cli tool" link="https://github.com/TheAlphaOnes/DevG" />
 
         </div>
     </div>

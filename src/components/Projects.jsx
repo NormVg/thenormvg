fe240d4933bd8da0aa39278dceeb725f20e2f346
NormVg <87635928+NormVg@users.noreply.github.com>
2024-03-14 19:32:24 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ export default function Projects() {
             <ProjectsCard name="Batman ChatRoom" disc="Batman 2022 inspired terminal like web Chatroom" link="https://batmanchatroom.onrender.com/" />
             <ProjectsCard name="Dusic" disc="A youtube based music player which I made because I hated ads" link="https://dusic.netlify.app/" />
             <ProjectsCard name="ChatBotGpt" disc="A gpt-2 based custom chatbot which can be finetuned" link="https://github.com/NormVg/ChatBotGpt" />
-            <ProjectsCard name="DevG" disc="A general purpose everyday developer cli tool" link="https://github.com/TheAlphaOnes/DevG" />
+{/*             <ProjectsCard name="DevG" disc="A general purpose everyday developer cli tool" link="https://github.com/TheAlphaOnes/DevG" /> */}
 
         </div>
     </div>

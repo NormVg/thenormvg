3cdfd436dfd5b2bb41d4c96215188ffdeff4468d
vishnu gupta <thenormvg@gmail.com>
2024-04-14 18:50:05 +0530

    ram ram bhiyo editing website again

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,8 +11,9 @@ export default function Projects() {
             <ProjectsCard name="Batman ChatRoom" disc="Batman 2022 inspired terminal like web Chatroom" link="https://batmanchatroom.onrender.com/" />
             <ProjectsCard name="Dusic" disc="A youtube based music player which I made because I hated ads" link="https://dusic.netlify.app/" />
             <ProjectsCard name="ChatBotGpt" disc="A gpt-2 based custom chatbot which can be finetuned" link="https://github.com/NormVg/ChatBotGpt" />
-{/*             <ProjectsCard name="DevG" disc="A general purpose everyday developer cli tool" link="https://github.com/TheAlphaOnes/DevG" /> */}
-
+            <ProjectsCard name="DevG" disc="The Best Developer CLI tool ever created" link="https://github.com/TheAlphaOnes/DevG" />
+            <ProjectsCard name="Eventually" disc="Eventually is a reimagining of journaling , dynamically provide Nostalgia and Calmness , detached from internet, for you to be you and help you grow" link="https://github.com/TheAlphaOnes/EventuallyApp" />
+            <ProjectsCard name="Grim.Vex" disc="Arc is Shit, I created my own browser" link="https://github.com/NormVg/Grim" />
         </div>
     </div>
   )

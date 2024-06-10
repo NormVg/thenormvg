db3496869d8e764850447c58f52cda54b11be2d2
Vishnu <thenormvg@gmail.com>
2024-06-10 11:37:03 +0530

    bhai abhi intro edit mara ha

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,11 +9,11 @@ export default function Projects() {
         <h1>My Projects</h1>
         <div id='pj-list'>
             <ProjectsCard name="Batman ChatRoom" disc="Batman 2022 inspired terminal like web Chatroom" link="https://batmanchatroom.onrender.com/" />
+            <ProjectsCard name="DevG" disc="The Best Developer CLI tool ever created" link="https://github.com/TheAlphaOnes/DevG" />
+            <ProjectsCard name="Grim.Vex" disc="Arc is Shit, I created my own browser" link="https://github.com/NormVg/Grim" />
+            <ProjectsCard name="Eventually" disc="Eventually is a reimagining of journaling , dynamically provide Nostalgia and Calmness , detached from internet, for you to be you and help you grow" link="https://github.com/TheAlphaOnes/EventuallyApp" />
             <ProjectsCard name="Dusic" disc="A youtube based music player which I made because I hated ads" link="https://dusic.netlify.app/" />
             <ProjectsCard name="ChatBotGpt" disc="A gpt-2 based custom chatbot which can be finetuned" link="https://github.com/NormVg/ChatBotGpt" />
-            <ProjectsCard name="DevG" disc="The Best Developer CLI tool ever created" link="https://github.com/TheAlphaOnes/DevG" />
-            <ProjectsCard name="Eventually" disc="Eventually is a reimagining of journaling , dynamically provide Nostalgia and Calmness , detached from internet, for you to be you and help you grow" link="https://github.com/TheAlphaOnes/EventuallyApp" />
-            <ProjectsCard name="Grim.Vex" disc="Arc is Shit, I created my own browser" link="https://github.com/NormVg/Grim" />
         </div>
     </div>
   )

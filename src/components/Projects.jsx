ba95b81aae3af153854b97321c6f55b6e66be455
Vishnu <thenormvg@gmail.com>
2024-06-14 22:47:06 +0530

    gg

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
-import ProjectsCard from './ProjectsCard'
+import React from "react";
+import ProjectsCard from "./ProjectsCard";
 
-import "../css/ProjectsCSS.css"
+import "../css/ProjectsCSS.css";
 
 export default function Projects() {
   return (
     <div>
-        <h1>My Projects</h1>
-        <div id='pj-list'>
-            <ProjectsCard name="Batman ChatRoom" disc="Batman 2022 inspired terminal like web Chatroom" link="https://batmanchatroom.onrender.com/" />
-            <ProjectsCard name="DevG" disc="The Best Developer CLI tool ever created" link="https://github.com/TheAlphaOnes/DevG" />
-            <ProjectsCard name="Grim.Vex" disc="Arc is Shit, I created my own browser" link="https://github.com/NormVg/Grim" />
-            <ProjectsCard name="Eventually" disc="Eventually is a reimagining of journaling , dynamically provide Nostalgia and Calmness , detached from internet, for you to be you and help you grow" link="https://github.com/TheAlphaOnes/EventuallyApp" />
-            <ProjectsCard name="Dusic" disc="A youtube based music player which I made because I hated ads" link="https://dusic.netlify.app/" />
-            <ProjectsCard name="ChatBotGpt" disc="A gpt-2 based custom chatbot which can be finetuned" link="https://github.com/NormVg/ChatBotGpt" />
-        </div>
+      <h1>My Projects</h1>
+      <div id="pj-list">
+        <ProjectsCard
+          name="Batman ChatRoom"
+          disc="Batman 2022 inspired terminal like web Chatroom"
+          link="https://batmanchatroom.vercel.app/"
+        />
+        <ProjectsCard
+          name="DevG"
+          disc="The Best Developer CLI tool ever created"
+          link="https://github.com/TheAlphaOnes/DevG"
+        />
+        <ProjectsCard
+          name="Grim.Vex"
+          disc="Arc is Shit, I created my own browser"
+          link="https://github.com/NormVg/Grim"
+        />
+        <ProjectsCard
+          name="Kraken"
+          disc="'NEW' code editor, made with new flavour, design pattern and theory "
+          link="https://github.com/NormVg/Kraken"
+        />
+        <ProjectsCard
+          name="Eventually"
+          disc="Eventually is a reimagining of journaling , dynamically provide Nostalgia and Calmness , detached from internet, for you to be you and help you grow"
+          link="https://github.com/TheAlphaOnes/EventuallyApp"
+        />
+        <ProjectsCard
+          name="Dusic"
+          disc="A youtube based music player which I made because I hated ads"
+          link="https://dusic.netlify.app/"
+        />
+        <ProjectsCard
+          name="ChatBotGpt"
+          disc="A gpt-2 based custom chatbot which can be finetuned"
+          link="https://github.com/NormVg/ChatBotGpt"
+        />
+      </div>
     </div>
-  )
+  );
 }

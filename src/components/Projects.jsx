58a76cd2a95902b03b4c58017050260bd9353a76
Vishnu <thenormvg@gmail.com>
2024-07-27 18:55:14 +0530

    jam added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,42 +8,6 @@ export default function Projects() {
     <div>
       <h1>My Projects</h1>
       <div id="pj-list">
-        {/* <ProjectsCard
-          name="Batman ChatRoom"
-          disc="Batman 2022 inspired terminal like web Chatroom"
-          link="https://batmanchatroom.vercel.app/"
-        />
-        <ProjectsCard
-          name="DevG"
-          disc="The Best Developer CLI tool ever created"
-          link="https://github.com/TheAlphaOnes/DevG"
-        />
-        <ProjectsCard
-          name="Grim.Vex"
-          disc="Arc is Shit, I created my own browser"
-          link="https://github.com/NormVg/Grim"
-        />
-        <ProjectsCard
-          name="Kraken"
-          disc="'NEW' code editor, made with new flavour, design pattern and theory "
-          link="https://github.com/NormVg/Kraken"
-        />
-        <ProjectsCard
-          name="Eventually"
-          disc="Eventually is a reimagining of journaling , dynamically provide Nostalgia and Calmness , detached from internet, for you to be you and help you grow"
-          link="https://github.com/TheAlphaOnes/EventuallyApp"
-        />
-        <ProjectsCard
-          name="Dusic"
-          disc="A youtube based music player which I made because I hated ads"
-          link="https://dusic.netlify.app/"
-        />
-        <ProjectsCard
-          name="ChatBotGpt"
-          disc="A gpt-2 based custom chatbot which can be finetuned"
-          link="https://github.com/NormVg/ChatBotGpt"
-        /> */}
-
         <ProjectCardNew
           title="Batman ChatRoom"
           content="Batman 2022 inspired terminal like web Chatroom"
@@ -59,17 +23,24 @@ export default function Projects() {
         /> */}
 
         <ProjectCardNew
-          title="Grim.Vex"
-          content="Arc is Shit, I created my own browser"
-          link="https://github.com/NormVg/Grim"
-          imag="grim.gif"
-        />
-
-        <ProjectCardNew
           title="Kraken"
           content="'NEW' code editor, made with new flavour, design pattern and theory "
           link="https://github.com/NormVg/Kraken"
           imag="kraken.gif"
+        />
+
+        <ProjectCardNew
+          title="JAM"
+          content="Just AppImage Manager for linux"
+          link="https://github.com/TheAlphaOnes/jam"
+          imag="def.png"
+        />
+
+        <ProjectCardNew
+          title="Grim.Vex"
+          content="Arc is Shit, I created my own browser"
+          link="https://github.com/NormVg/Grim"
+          imag="grim.gif"
         />
 
         <ProjectCardNew
